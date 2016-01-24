8d19a2c93c195eca1a2c02c243035503f1c455da
David Grayston <davidgrayston@googlemail.com>
2016-01-24 23:58:57 +0000

    Supporting global markup e.g. Organization

--- rdfa_xpath.api.php
+++ rdfa_xpath.api.php
@@ -7,18 +7,44 @@
 function hook_rdfa_xpath_mappings() {
   $node_base = "//*[contains(concat(' ', @class, ' '), ' node-full ')]";
   $rdfa_mappings = array(
+    // Global example using the vocab attribute.
+    array(
+      'type' => 'global',
+      'mappings' => array(
+        array(
+          'xpath' => "//*[contains(concat(' ', @class, ' '), ' logo-wrapper ')]",
+          'attributes' => array(
+            'vocab' => 'http://schema.org/',
+            'typeof' => 'Organization',
+          ),
+        ),
+        array(
+          'xpath' => "//a[contains(concat(' ', @class, ' '), ' site-logo ')]",
+          'attributes' => array(
+            'property' => 'url',
+          ),
+        ),
+        array(
+          'xpath' => "//a[contains(concat(' ', @class, ' '), ' site-logo ')]//img",
+          'attributes' => array(
+            'property' => 'logo',
+          ),
+        ),
+      ),
+    ),
+    // Node example using a prefix, useful when mixing vocabularies.
     array(
       'type' => 'node',
       'bundle' => 'article',
       'mappings' => array(
         array(
-          'xpath' => "//body",
+          'xpath' => "//*[@id='page']",
           'attributes' => array(
             'prefix' => 'schema:http://schema.org/',
           ),
         ),
         array(
-          'xpath' => "//*[@id = 'page']",
+          'xpath' => "//*[@id='main']",
           'attributes' => array(
             'typeof' => 'schema:Article',
           ),
@@ -46,7 +72,7 @@ function hook_rdfa_xpath_mappings() {
           'xpath' => $node_base . "//*[contains(concat(' ', @class, ' '), ' field-name-field-published-date ')]//*[contains(concat(' ', @class, ' '), ' date-display-single ')]",
           'attributes' => array(
             'property' => 'schema:datePublished',
-            'content' => '[node:field_published_date]'
+            'content' => '[node:field-published-date]',
           ),
         ),
         array(
